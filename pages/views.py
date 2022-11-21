ba2249c8e50ccfaaccd9c1924d7b49e951374f79
adham-66 <adhamhewalla66@gmail.com>
2022-11-20 17:54:51 -0800

    add pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
+def home(request):
+    context = {
+        'products':Product.objects.all()
+    }
+    return render(request, 'pages/home.html', context)
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def coffee(request):
+    return render(request, 'pages/coffee.html')
