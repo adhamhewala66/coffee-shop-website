4e9cb99cea2b122772adb2b083ab0f9ea31c2eab
adham-66 <adhamhewalla66@gmail.com>
2022-11-21 06:58:01 -0800

    add search & style adminstration

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ProductList, ProductDetail, search
+from .views import ProductList, filter, ProductDetail, search
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductList.as_view(), name='product-list'),
+    path('s/', filter, name='filter'),
     path('<int:pk>/', ProductDetail.as_view(), name='product-detail'),
-    path('search/', search, name='search')
+    path('search/', search, name='search'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,3 +10,42 @@ class ProductDetail(DetailView):
     
 def search(request):
     return render(request, 'products/search.html')
+
+def filter(request):
+    products = Product.objects.all()
+    name = None
+    desc = None
+    sfrom = None
+    sto = None
+    sensitive = None
+    if 'sensitive' in request.GET:
+        sensitive = request.GET['sensitive']
+        if not sensitive:
+            sensitive = 'off'
+            
+    if 'sname' in request.GET:
+        name = request.GET['sname']
+        if name:
+            if sensitive == 'on':
+                products = products.filter(name__contains=name)
+            else:
+                products = products.filter(name__icontains=name)
+                
+    if 'sdescription' in request.GET:
+        desc = request.GET['sdescription']
+        if desc:
+            if sensitive == 'on':
+                products = products.filter(desc__contains=desc)
+            else:
+                products = products.filter(desc__icontains=desc)
+            
+    if 'sfrom' in request.GET and 'sto' in request.GET:
+        sfrom = request.GET['sfrom']
+        sto = request.GET['sto']
+        if sfrom and sto:
+            if sfrom.isdigit() and sto.isdigit():
+                products = products.filter(price__gte=sfrom, price__lte=sto)
+    context={
+        'products':products
+    }
+    return render(request, 'products/product_filter.html', context)
